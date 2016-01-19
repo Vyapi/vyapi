545751a20c7e07623ad868349819ffb96cf4eef6
Asheesh Pathak <eroinroderigo@gmail.com>
2016-01-19 21:12:54 +0530

    DB complete

--- src/app/action/action.controller.js
+++ src/app/action/action.controller.js
@@ -2,11 +2,10 @@ export class ActionController {
     constructor($firebaseArray) {
         'ngInject';
 
-    var myDataRef = new Firebase('https://torrid-fire-8763.firebaseio.com/');
-    myDataRef.set("Himanshu");
-    /*
-      myDataRef.on('child_added', function(snapshot) {
-      var message = snapshot.val();
-    console.log(message);
-      });*/
+        var myDataRef = new Firebase('https://torrid-fire-8763.firebaseio.com/');
+        myDataRef.set({
+            name: "name1",
+            text: "text1"
+        });
+    }
 }
