9fa127b0f709a72a6bcc13bf8a8f1390064a296b
monicagangwar <monicagangwar10@gmail.com>
2016-01-19 14:34:43 +0530

    all the necessary modules

---
+++ src/app/index.route.js
@@ -0,0 +1,12 @@
+export function routerConfig ($routeProvider) {
+  'ngInject';
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/main/main.html',
+      controller: 'MainController',
+      controllerAs: 'main'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}
