8d68a3f86952050249268a2968e23e42aeee4772
Pronnoy Goswami <pronnoy.goswami@gmail.com>
2016-01-19 17:41:13 +0530

    Cloned a user folder similar to main folder

---
+++ src/app/user/user.controller.js
@@ -0,0 +1,12 @@
+export class UserController {
+  constructor ($timeout, webDevTec, toastr) {
+    'ngInject';
+
+    this.awesomeThings = [];
+    this.classAnimation = '';
+    this.creationDate = 1453195760504;
+    this.toastr = toastr;
+
+    this.activate($timeout, webDevTec);
+  }
+}
