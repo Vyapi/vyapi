c3c6bd4a7e109dda41807a3096a3c1cd5f868b68
monicagangwar <monicagangwar10@gmail.com>
2016-01-19 18:32:06 +0530

    fetching data

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -1,32 +1,8 @@
 export class DashboardController {
-  constructor ($timeout, webDevTec, toastr) {
+  constructor ($firebaseObject) {
     'ngInject';
-
-    this.awesomeThings = [];
-    this.classAnimation = '';
-    this.creationDate = 1453195760504;
-    this.toastr = toastr;
-
-    this.activate($timeout, webDevTec);
-  }
-
-  activate($timeout, webDevTec) {
-    this.getWebDevTec(webDevTec);
-    $timeout(() => {
-      this.classAnimation = 'rubberBand';
-    }, 4000);
-  }
-
-  getWebDevTec(webDevTec) {
-    this.awesomeThings = webDevTec.getTec();
-
-    angular.forEach(this.awesomeThings, (awesomeThing) => {
-      awesomeThing.rank = Math.random();
-    });
-  }
-
-  showToastr() {
-    this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-    this.classAnimation = '';
+		var rootRef = new Firebase('https://vyapi.firebaseio.com/');
+		this.data = $firebaseObject(rootRef);
+		console.log(this.data);
   }
 }
