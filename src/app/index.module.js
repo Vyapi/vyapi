83b54165f7d7f94d6213871a8c16b0c1ea5318c8
Pronnoy Goswami <pronnoy.goswami@gmail.com>
2016-01-19 17:47:18 +0530

    Cloned a user folder similar to main folder

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,6 +4,7 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
+import { UserController } from './user/user.controller';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
@@ -18,5 +19,6 @@ angular.module('vyapi', ['ui.router', 'toastr'])
   .service('githubContributor', GithubContributorService)
   .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
+  .controller('UserController', UserController)
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective);
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,11 +1,17 @@
 export function routerConfig ($stateProvider, $urlRouterProvider) {
   'ngInject';
   $stateProvider
-    .state('home', {
+    /*.state('home', {
       url: '/',
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
+    })*/
+    .state('home', {
+      url: '/',
+      templateUrl: 'app/user/user.html',
+      controller: 'UserController',
+      controllerAs: 'user'
     });
 
   $urlRouterProvider.otherwise('/');
