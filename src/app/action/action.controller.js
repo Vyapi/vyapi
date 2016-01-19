2b3ba7f2a919dede8916f74b49610372435a1c05
Asheesh Pathak <eroinroderigo@gmail.com>
2016-01-19 21:28:34 +0530

    DB complete

--- src/app/action/action.controller.js
+++ src/app/action/action.controller.js
@@ -3,9 +3,14 @@ export class ActionController {
         'ngInject';
 
         var myDataRef = new Firebase('https://torrid-fire-8763.firebaseio.com/');
-        myDataRef.set({
-            name: "name1",
-            text: "text1"
-        });
+        /*myDataRef.push("himanshu");
+        myDataRef.push("mayuresh");
+        myDataRef.push("abhimanyu");
+        myDataRef.push("prakhar");*/
+        myDataRef.on('child_added',function(snapshot){
+        //console.log('sdds');
+        var message=snapshot.val();
+        //console.log(message);
+      });
     }
 }
