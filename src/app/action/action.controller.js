665260184b3f42a9bea068bab0135e259f52f7b2
Asheesh Pathak <eroinroderigo@gmail.com>
2016-01-19 16:21:09 +0530

    updated controller definition and directory

---
+++ src/app/action/action.controller.js
@@ -0,0 +1,13 @@
+export class ActionController {
+  constructor ($timeout, webDevTec, toastr) {
+    'ngInject';
+
+    this.awesomeThings = [];
+    this.classAnimation = '';
+    this.creationDate = 1453195760504;
+    this.toastr = toastr;
+
+    this.activate($timeout, webDevTec);
+  }
+
+}
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -6,6 +6,12 @@ export function routerConfig ($stateProvider, $urlRouterProvider) {
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
+    })
+    .state('action', {
+      url: '/action',
+      templateUrl: 'app/action/action.html',
+      controller: 'ActionController',
+      controllerAs: 'action'
     });
 
   $urlRouterProvider.otherwise('/');
