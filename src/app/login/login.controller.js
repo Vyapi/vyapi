68b0a158a90b8aaaef2da76acbe5d0aba8b089e1
himanshusingh2407 <himanshu@admins-MacBook-Pro-6.local>
2016-01-20 02:34:23 +0530

    added sign out check and redirect

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,10 +1,9 @@
 var ref = new Firebase("https://vyapi-userauth.firebaseio.com/");
-
-        var authData = ref.getAuth();
-
-        if (authData) {
-          console.log("User " + authData.uid + " is logged in with " + authData.provider);
-        } else {
-               window.location="https://vyapi.firebaseapp.com";
-        }
+var authData = ref.getAuth();
+if (authData) {
+  console.log("User " + authData.uid + " is logged in with " + authData.provider);
+}
+else {
+  window.location="https://vyapi.firebaseapp.com";
+}
 
