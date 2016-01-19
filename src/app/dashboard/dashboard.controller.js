7387696f4548a0ff4635b39d53764217181f62ea
monicagangwar <monicagangwar10@gmail.com>
2016-01-20 01:38:12 +0530

    fetching room IDs

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -1,6 +1,7 @@
 export class DashboardController {
   constructor () {
     'ngInject';
+    		var userID = "monica";
     		var rootURL = 'https://mock-vyapi.firebaseio.com';
 		var rootRef = new Firebase(rootURL);
 		var membersRef = new Firebase(rootURL + '/members');
@@ -9,33 +10,23 @@ export class DashboardController {
 		var roomURLs = [];
 		membersRef.once("value",function(snapshot){
 				if(snapshot.exists()){
-					console.log("members available");
+					//console.log("members available");
 					var members = snapshot.val();
-					var roomLength = Object.keys(members).length;
-					var key = Object.keys(members);
-					console.log(key);
-					console.log(roomLength);
-					
+					for(var membersKey in members){
+						var membersVal = members[membersKey];
+						for(var key in membersVal){
+							//console.log(key,userID,membersVal[key])
+							if(key === userID && membersVal[key]){
+								roomURLs.push(membersKey);
+							}
+						}
+						
+					}
+					console.log(roomURLs);
+
 				}
-				else
-				{
-					return roomURLs;
-				}
+
 		});
-		/*
-		var data = $firebaseObject(rootRef);
-		console.log(data.toString().length);
-		var roomURLS = [];
-		if(data.length == 0){
-			console.log("empty");
-			createNewRoom();
-			return roomURLs;
-		}
-		else{
-			var memberRef = new Firebase(rootURL + '/members');
-			console.log("we'll see");
-		}
-		*/
 		function createNewRoom(){
 			for(var i=1;i<=10;i++){
 				membersRef.push({'monica' : 'true', 'ayushi' : 'true'});
