6e467666bfad056d2669b32cf492a75b182b5828
diwakar <diwakar.s.maurya@gmail.com>
2016-01-21 00:08:50 +0530

    bug fixes in presence system

--- src/app/user/user.controller.js
+++ src/app/user/user.controller.js
@@ -1,9 +1,10 @@
 export class UserController {
-  constructor() {
+  constructor($firebaseArray) {
     'ngInject';
-    this.onlineUsers = [];
     var appURL = "https://incandescent-inferno-4532.firebaseio.com/";
     var onlineUsersRef = new Firebase(appURL + "onlineUsers");
+    this.onlineUsers = [];
+    var t = $firebaseArray(onlineUsersRef); //no idea what this line does, but removing this criples whole application
     this.goOnline = function() {
       //get uid from cookie or from wherever the authentication teams decides
       var uid = Math.floor(Math.random() * 999999999) + 1;
@@ -12,35 +13,34 @@ export class UserController {
       console.log("My uid is: " + uid);
       //check self connection state
       var connectedRef = new Firebase(appURL + ".info/connected");
-      connectedRef.on("value", function(snap) {
+      connectedRef.on("value", (snap) => {
         if (snap.val() === true) {
           console.log("connected");
           //register self as online
-          onlineUsersRef.child(uid.toString()).set("1");
+          (new Firebase(encodeURI(appURL + "userAuthInfo/" + uid + "/google/cachedUserProfile/given_name/"))).once("value", (value) => {
+            var userName = value.val();
+            (new Firebase(encodeURI(appURL + "userAuthInfo/" + uid + "/google/profileImageURL/"))).once("value", (value) => {
+              var profileImageURL = value.val();
+              onlineUsersRef.child(uid).set({name: userName, photo : profileImageURL});
+            });
+          });
+          
           //setup offline mechanism when going offline
-          onlineUsersRef.child(uid.toString()).onDisconnect().remove();
+          onlineUsersRef.child(uid).onDisconnect().remove();
         } else {
           console.log("not connected");
         }
       });
+      
       //user came online
-      onlineUsersRef.on('child_added', function(userId) {
+      onlineUsersRef.on('child_added', (userId) => {
         console.log("User: " + userId.key() + " came online");
-        var userName, profileImageURL;
-        (new Firebase(encodeURI(appURL + "userAuthInfo/" + userId + "/google/cachedUserProfile/given_name/"))).once("value", function(value) {
-          userName = value.val();
-        });
-        (new Firebase(encodeURI(appURL + "userAuthInfo/" + userId + "/google/profileImageURL/"))).once("value", function(value) {
-          profileImageURL = value.val();
-        });
-        this.onlineUsers.push({
-          "name": userName
-        });
+        this.onlineUsers.push(userId.val());
+        console.table(this.onlineUsers);
       });
       //user went offline
       onlineUsersRef.on('child_removed', function(userId) {
         console.log("User: " + userId.key() + " went offline");
-        pop(userId);
       });
     }
     this.goOnline();
