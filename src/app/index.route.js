d2078555711f2ffb77a4e39d9c32718448907e78
monicagangwar <monicagangwar10@gmail.com>
2016-01-20 17:34:42 +0530

    added dashboard state

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,21 +1,22 @@
 export function routerConfig ($stateProvider, $urlRouterProvider) {
   'ngInject';
   $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: 'app/dashboard/dashboard.html',
-      //controller: 'MainController',
-      controller:'DashboardController',
-      controllerAs: 'dashboard'
-      templateUrl: 'app/login/login.html',
-      controller: 'LoginController',
-      controllerAs: 'login'
-    }).state('board', {
-      url: '/board',
-      templateUrl: 'app/board/board.html',
-      controller: 'BoardController',
-      controllerAs: 'board'
-    });
+  .state('home',{
+    url:'/',
+    templateUrl: 'app/login/login.html',
+    controller: 'LoginController',
+    controllerAs: 'login'
+    }).state('dashboard', {
+    url: '/dashboard',
+    templateUrl: 'app/dashboard/dashboard.html',
+    controller:'DashboardController',
+    controllerAs: 'dashboard'
+  }).state('board', {
+    url: '/board',
+    templateUrl: 'app/board/board.html',
+    controller: 'BoardController',
+    controllerAs: 'board'
+  });
 
   $urlRouterProvider.otherwise('/');
 }
