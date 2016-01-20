eb07493af7777a2161ac02d4ccd7da4165df3000
himanshusingh2407 <himanshu@admins-MacBook-Pro-6.local>
2016-01-21 01:10:25 +0530

    corrected multiple login problem

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,32 +1,42 @@
 export class LoginController {
-  constructor ($firebaseAuth) {
+  constructor ($firebaseObject,$firebaseAuth) {
     'ngInject';
-    this.test="Good Morning";
     this.ref = new Firebase("https://vyapi.firebaseio.com");
-    this.fa = $firebaseAuth;
-  }
-  firebaseAuthLogin() {
-    this.authObj = this.fa(this.ref);
-    this.authObj.$authWithOAuthPopup("google").then((authData) =>{
-      console.log("Logged in as:", authData.uid);
-    }).catch(function(error) {
-      console.error("Authentication failed:", error);
-    });
-  }
 
-  firebaseAuthStatus(){
-    this.authObj = this.fa(this.ref);
-    var authData = this.authObj.$getAuth();
-    console.log("this works");
-    if (authData) {
-      console.log("Logged in as:", authData.uid);
-    } else {
-      console.log("Logged out");
+    this.firebaseAuthLogin = function(){
+      this.authObj = $firebaseAuth(this.ref);
+      var authData = this.authObj.$getAuth();
+      if (authData) {
+        console.log("Already Logged in as:", authData.uid);
+      } else {
+        this.authObj.$authWithOAuthPopup("google").then(function(authData) {
+          console.log("Logged in as:", authData.uid);
+        }).catch(function(error) {
+          console.error("Authentication failed:", error);
+        });
+      }
+    }
+
+    this.firebaseAuthStatus = function(){
+      this.authObj = $firebaseAuth(this.ref);
+      var authData = this.authObj.$getAuth();
+      if (authData) {
+        console.log("Logged in as:", authData.uid);
+      } else {
+        console.log("Logged out");
+      }
+    }
+
+    this.firebaseAuthlogout = function(){
+      this.authObj = $firebaseAuth(this.ref);
+      var authData = this.authObj.$getAuth();
+      if (authData) {
+        this.authObj.$unauth();
+        console.log("Successfully Logged out");
+      } else {
+        console.log("Already Logged out");
+      }
     }
   }
 
-  firebaseAuthlogout(){
-    this.authObj = this.fa(this.ref);
-    this.authObj.$unauth()
-  }
 }
