570d6e276f45056938f4d9263317abd92250a26c
vatsalya25 <vatsalya25@gmail.com>
2016-01-20 12:33:22 +0530

    live text in board controller

--- src/app/board/board.controller.js
+++ src/app/board/board.controller.js
@@ -1,24 +1,25 @@
 export class boardController {
-  constructor ($timeout, webDevTec, toastr) {
+  constructor ($firebaseArray) {
     'ngInject';
 
-    var ref = new Firebase('flickering-fire-3902.firebaseIO.com');
-    $('#messageInput').keypress(function (e) {
-      if (e.keyCode == 13) {
-        var name = $('#nameInput').val();
-        var text = $('#messageInput').val();
-        ref.push({name: name, text: text});
-        $('#messageInput').val('');
-      }
-    });
-    ref.on('child_added', function(snapshot) {
-      var message = snapshot.val();
-      displayChatMessage(message.name, message.text);
-    });
-    function displayChatMessage(name, text) {
-      $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
-      $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-    };
+    var ref = new Firebase("flickering-fire-3902.firebaseIO.com");
 
+    this.messages = $firebaseArray(ref);
+
+    this.addMessage = function() {
+
+      if (this.msg) {
+
+        var name = this.name || "anonymous";
+
+        this.messages.$add({
+          from: name,
+          body: this.msg
+        });
+
+        this.msg = "";
+      }
+    }
   }
+
 }
--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,13 +4,13 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
-import { boardController } from './board/board.controller';
+import { BoardController } from './board/board.controller';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
-angular.module('vyapi', ['ui.router', 'toastr'])
+angular.module('vyapi', ['ui.router', 'toastr', 'firebase'])
   .constant('malarkey', malarkey)
   .constant('moment', moment)
   .config(config)
@@ -19,6 +19,6 @@ angular.module('vyapi', ['ui.router', 'toastr'])
   .service('githubContributor', GithubContributorService)
   .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
-  .controller('boardController', boardController)
+  .controller('BoardController', BoardController)
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective);
