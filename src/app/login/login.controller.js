8277f50ab0a67f7bc45df206db0ab8f06002a34e
Mayuresh Waykole <mayuresh2212@gmail.com>
2016-01-20 16:26:54 +0530

    clecaning up the controller

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,33 +1,32 @@
 export class LoginController {
-  constructor ($firebaseObject,$firebaseAuth) {
+  constructor ($firebaseAuth) {
     'ngInject';
     this.test="Good Morning";
     this.ref = new Firebase("https://vyapi.firebaseio.com");
+    this.fa = $firebaseAuth;
+  }
+  firebaseAuthLogin() {
+    this.authObj = this.fa(this.ref);
+    this.authObj.$authWithOAuthPopup("google").then((authData) =>{
+      console.log("Logged in as:", authData.uid);
+    }).catch(function(error) {
+      console.error("Authentication failed:", error);
+    });
+  }
 
-    this.firebaseAuthLogin = function(){
-      this.authObj = $firebaseAuth(this.ref);
-      this.authObj.$authWithOAuthPopup("google").then(function(authData) {
-        console.log("Logged in as:", authData.uid);
-      }).catch(function(error) {
-        console.error("Authentication failed:", error);
-      });
-    }
-
-    this.firebaseAuthStatus = function(){
-      this.authObj = $firebaseAuth(this.ref);
-      var authData = this.authObj.$getAuth();
-      console.log("this works");
-      if (authData) {
-        console.log("Logged in as:", authData.uid);
-      } else {
-        console.log("Logged out");
-      }
-    }
-
-    this.firebaseAuthlogout = function(){
-      this.authObj = $firebaseAuth(this.ref);
-      this.authObj.$unauth()
+  firebaseAuthStatus(){
+    this.authObj = this.fa(this.ref);
+    var authData = this.authObj.$getAuth();
+    console.log("this works");
+    if (authData) {
+      console.log("Logged in as:", authData.uid);
+    } else {
+      console.log("Logged out");
     }
   }
 
+  firebaseAuthlogout(){
+    this.authObj = this.fa(this.ref);
+    this.authObj.$unauth()
+  }
 }
