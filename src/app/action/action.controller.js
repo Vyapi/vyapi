347b5f59b95d844b1c13d04e817ea3fed80dc416
Asheesh Pathak <eroinroderigo@gmail.com>
2016-01-20 17:55:08 +0530

    Complete

--- src/app/action/action.controller.js
+++ src/app/action/action.controller.js
@@ -2,44 +2,54 @@ export class ActionController {
     constructor($firebaseArray) {
         'ngInject';
 
-      this.items = [ ];
       this.person='';
+      this.items='';
       this.input='';
       this.roomID='101';
       this.assignee=['mayurehs','abhi','aish'];
       this.myDataRef = new Firebase('https://torrid-fire-8763.firebaseio.com/');
-       /* myDataRef.set('');
-        myDataRef.push("himanshu");
-        myDataRef.push("mayuresh");
-        myDataRef.push("abhimanyu");
-        myDataRef.push("prakhar");*/
+
         this.myDataRef.on('child_added',function(snapshot){
-        //console.log('sdds');
         var message=snapshot.val();
+        //console.log(message);
       });
+
+
+        this.myDataRef.on('child_added',(snapshot)=>{
+         //console.log('sdds');
+         let actions=snapshot.val();
+         this.items = _.map(actions,(action,key)=>{
+           action.key=key;
+           return action;
+         });
+         console.table(this.items);
+         //console.log(this.items[0].key);
+       });
+
   }
 
     add()
     {
-      this.items.push(this.input);
+      this.items.push({task:this.input,name:this.person,roomid:this.roomID});
       //console.log(this.roomID);
       //this.myDataRef.push(this.input+"  "+(this.person==''?'not assigned to anyone':this.person)+" "+this.roomID);
-      //this.myDataRef.child('action').push({task:this.input,name:this.person,roomid:this.roomID});
+      this.myDataRef.child('action').push({task:this.input,name:this.person,roomid:this.roomID});
       //myDataRef.child("users/"+ authData.uid).set(authData);
       this.input = '';
-      //this.person='';
+      this.person='';
     }
 
     change($person)
     {
-      console.log($person);
+      //console.log($person);
       this.person=$person;
     }
 
     remove($index)
     {
       console.log($index);
-      this.items.splice($index.$index, 1);
+      this.myDataRef.child('action/'+this.items[$index].key).remove();
+      this.items.splice($index, 1);
       //this.myDataRef.child('action/'+$index)
     }
 
