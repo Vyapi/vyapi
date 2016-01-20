dd32dadec91140762f634d65e33d60c19094c4fb
Mayuresh Waykole <mayuresh2212@gmail.com>
2016-01-20 15:46:01 +0530

    changed double inverted commas to single inverted commas

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -10,7 +10,7 @@ import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service'
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
-angular.module('vyapi', ['ui.router', 'toastr',"firebase"])
+angular.module('vyapi', ['ui.router', 'toastr','firebase'])
   .constant('malarkey', malarkey)
   .constant('moment', moment)
   .config(config)
