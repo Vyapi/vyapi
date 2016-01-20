8cb414c23b03f9da0a5bdcc0f7df306d3b8379e8
monicagangwar <monicagangwar10@gmail.com>
2016-01-20 15:47:57 +0530

    removed grabage files and reformatted

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -1,25 +1,25 @@
 export class DashboardController {
-  constructor ($firebaseArray){
-    'ngInject';
-    	var userID = "monica";
-    	var roomsURL = 'https://mock-vyapi.firebaseio.com/rooms';
-	var roomsRef = new Firebase(roomsURL);
-	this.rooms = [];
-	this.createRoom = function(userID){
-		var roomName=prompt('Room Name','Default');
-		console.log(userID,roomName);
-		roomsRef.push({roomName : roomName, ownedBy : userID});
-	};
-	var data = $firebaseArray(roomsRef);
-	roomsRef.orderByChild("ownedBy").equalTo(userID).on("value",(snapshot)=>{
-		let rooms = snapshot.val();
-		rooms = _.map(rooms,(room,key,url)=>{
-			room.key = key;
-			room.url = roomsRef + '/' + key;
-			return room;
+	constructor ($firebaseArray){
+		'ngInject';
+		let userID = "monica";
+		let roomsURL = 'https://mock-vyapi.firebaseio.com/rooms';
+		let roomsRef = new Firebase(roomsURL);
+		this.rooms = [];
+		this.createRoom = function(userID){
+			let roomName=prompt('Room Name','Default');
+			//console.log(userID,roomName);
+			roomsRef.push({roomName : roomName, ownedBy : userID});
+		};
+		let data = $firebaseArray(roomsRef);
+		roomsRef.orderByChild("ownedBy").equalTo(userID).on("value",(snapshot)=>{
+			let rooms = snapshot.val();
+			rooms = _.map(rooms,(room,key,url)=>{
+				room.key = key;
+				room.url = roomsRef + '/' + key;
+				return room;
+			});
+			this.rooms = rooms;
+			//console.table(this.rooms);
 		});
-		this.rooms = rooms;
-		console.table(this.rooms);
-	});
-  }
+	}
 }
