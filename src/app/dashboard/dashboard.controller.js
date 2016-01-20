0b06351d555321ee3c22c29bbfd1383d10bed585
ayushinigam <ayushi@admins-MacBook-Pro-10.local>
2016-01-20 15:15:45 +0530

    New roooms with room name

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -5,7 +5,8 @@ export class DashboardController {
     	var roomsURL = 'https://mock-vyapi.firebaseio.com/rooms';
 	var roomsRef = new Firebase(roomsURL);
 	this.rooms = [];
-	this.createRoom = function(userID,roomName){
+	this.createRoom = function(userID){
+		var roomName=prompt('Room Name','Default');
 		console.log(userID,roomName);
 		roomsRef.push({roomName : roomName, ownedBy : userID});
 	};
