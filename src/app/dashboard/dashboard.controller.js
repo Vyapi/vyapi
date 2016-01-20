3109067822bed6903027eaa1a6f0ad1aaea5681c
monicagangwar <monicagangwar10@gmail.com>
2016-01-20 15:05:23 +0530

    added lodash functionality

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -10,7 +10,16 @@ export class DashboardController {
 		roomsRef.push({roomName : roomName, ownedBy : userID});
 	};
 	roomsRef.orderByChild("ownedBy").equalTo(userID).on("value",(snapshot)=>{
-		var rooms = Object.keys(snapshot.val());
+		let rooms = snapshot.val();
+		rooms = _.map(rooms,(room,key,url)=>{
+			room.key = key;
+			room.url = roomsRef + '/' + key;
+			return room;
+		});
+		this.rooms = rooms;
+		console.table(this.rooms);
+		console.table(rooms);
+		/*
 		this.rooms = {};
 		for(var key in rooms){
 			var name = snapshot.child(rooms[key] + '/roomName').val();
@@ -18,6 +27,7 @@ export class DashboardController {
 			this.rooms[name] = url;
 		}
 		console.log(this.rooms);
+		*/
 	});
   }
 }
