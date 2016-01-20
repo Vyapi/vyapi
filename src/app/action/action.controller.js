c902ca1e1d6236abed2a7f1dc3291977c17a9845
Asheesh Pathak <eroinroderigo@gmail.com>
2016-01-20 16:22:25 +0530

    fshdjf

--- src/app/action/action.controller.js
+++ src/app/action/action.controller.js
@@ -16,29 +16,31 @@ export class ActionController {
         this.myDataRef.on('child_added',function(snapshot){
         //console.log('sdds');
         var message=snapshot.val();
-        //console.log(message);
       });
   }
 
     add()
     {
-      this.items.push(this.input+"  "+(this.person==''?'not assigned to anyone':this.person));
+      this.items.push(this.input);
       //console.log(this.roomID);
       //this.myDataRef.push(this.input+"  "+(this.person==''?'not assigned to anyone':this.person)+" "+this.roomID);
-      this.myDataRef.child('action').push({task:this.input,name:this.person,roomid:this.roomID});
+      //this.myDataRef.child('action').push({task:this.input,name:this.person,roomid:this.roomID});
+      //myDataRef.child("users/"+ authData.uid).set(authData);
       this.input = '';
-      this.person='';
+      //this.person='';
     }
 
     change($person)
     {
-      //console.log($person);
+      console.log($person);
       this.person=$person;
     }
 
     remove($index)
     {
-      this.items.splice($index, 1);
+      console.log($index);
+      this.items.splice($index.$index, 1);
+      //this.myDataRef.child('action/'+$index)
     }
 
   }
