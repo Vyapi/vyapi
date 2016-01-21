a38e760e9fa2bc8fbf4c8bfd1b60a0eceed8e221
Mayuresh Waykole <mayuresh2212@gmail.com>
2016-01-21 09:45:56 +0530

    Added functions outside contructor to maintain context

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -2,43 +2,44 @@ export class LoginController {
   constructor ($firebaseObject,$firebaseAuth,$log) {
     'ngInject';
     this.ref = new Firebase("https://vyapi.firebaseio.com");
-
-    this.firebaseAuthLogin = function(){
-      this.authObj = $firebaseAuth(this.ref);
-      var authData = this.authObj.$getAuth();
-      if (authData) {
-        $log.log("Already Logged in as:", authData.uid);
-      } else {
-        this.authObj.$authWithOAuthPopup("google").then(function(authData) {
-          $log.log("111Logged in as:", authData.uid);
-          var ref = new Firebase("https://vyapi.firebaseio.com");
-          ref.child("users/"+ authData.uid).set(authData);          $log.log("Logged in as:", authData.uid);
-        }).catch(function(error) {
-          $log.error("Authentication failed:", error);
-        });
-      }
-    }
-
-    this.firebaseAuthStatus = function(){
-      this.authObj = $firebaseAuth(this.ref);
-      var authData = this.authObj.$getAuth();
-      if (authData) {
-        $log.log("Logged in as:", authData.uid);
-      } else {
-        $log.log("Logged out");
-      }
-    }
-
-    this.firebaseAuthlogout = function(){
-      this.authObj = $firebaseAuth(this.ref);
-      var authData = this.authObj.$getAuth();
-      if (authData) {
-        this.authObj.$unauth();
-        $log.log("Successfully Logged out");
-      } else {
-        $log.log("Already Logged out");
-      }
+    this.fa = $firebaseAuth;
+    this.clog = $log;
+  }
+  firebaseAuthLogin() {
+    this.authObj = this.fa(this.ref);
+    var authData = this.authObj.$getAuth();
+    if (authData) {
+      this.clog.log("Already Logged in as:", authData.uid);
+    } else {
+      this.authObj.$authWithOAuthPopup("google").then(function(authData) {
+        console.log("111Logged in as:", authData.uid);
+        var ref = new Firebase("https://vyapi.firebaseio.com");
+        ref.child("users/"+ authData.uid).set(authData);
+        console.log("Logged in as:", authData.uid);
+      }).catch(function(error) {
+        this.clog.error("Authentication failed:", error);
+      });
     }
   }
 
+  firebaseAuthStatus() {
+    this.authObj = this.fa(this.ref);
+    var authData = this.authObj.$getAuth();
+    if (authData) {
+      this.clog.log("Logged in as:", authData.uid);
+    } else {
+      this.clog.log("Logged out");
+    }
+  }
+
+  firebaseAuthlogout() {
+    this.authObj = this.fa(this.ref);
+    var authData = this.authObj.$getAuth();
+    if (authData) {
+      this.authObj.$unauth();
+      this.clog.log("Successfully Logged out");
+    } else {
+      this.clog.log("Already Logged out");
+    }
+  }
 }
