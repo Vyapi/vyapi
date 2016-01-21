50adecec5fd4ce82f38e10e591c4835ed0a13c80
monicagangwar <monicagangwar10@gmail.com>
2016-01-21 20:07:34 +0530

    all logic moved back to controller and no entry in members

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -1,29 +1,19 @@
 export class DashboardController {
-	constructor ($firebaseArray,dashboardService){
+	constructor ($firebaseArray){
 		'ngInject';
-		this.userID = '';
-		this.set_param(dashboardService);
 		let rootURL = 'https://vyapi.firebaseio.com/';
 		let roomsURL = rootURL + 'rooms';
 		let rootRef = new Firebase(rootURL);
 		let roomsRef = new Firebase(roomsURL);
 		let data = $firebaseArray(roomsRef);
+		let authData = rootRef.getAuth();
+		let userID = authData.uid;
 		this.rooms = [];
 		this.createRoom = function(){
 			let roomName=prompt('Room Name','Default');
 			//console.log(userID,roomName);
-			roomsRef.push({roomName : roomName, ownedBy : this.userID, members:[this.userID]});
+			roomsRef.push({roomName : roomName, ownedBy : userI});
 		};
-	}
-	set_param(dashboardService){
-		this.userID = dashboardService.getUserID();
-		this.rooms = dashboardService.getRooms();
-		console.table(this.rooms);
-		console.log(this.userID);
-		this.createRoom=function(){
-			this.create(dashboardService);
-		}
-       
 		roomsRef.orderByChild("ownedBy").equalTo(userID).on("value",(snapshot)=>{
 			let rooms = snapshot.val();
 			rooms = _.map(rooms,(room,key,url)=>{
@@ -34,19 +24,4 @@ export class DashboardController {
 			this.rooms = rooms;
 		});
 	}
-
-	create(dashboardService)
-	{
-		
-		var k=dashboardService.createRoom();
-		
-	}
-
-	abc(dashboardService)
-	
-		{
-			var t=dashboardService.getsrc();
-			console.log(t);
-		}
-	
 }
