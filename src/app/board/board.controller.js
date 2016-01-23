f30380f5d64b007930d0e8d6889d79e0175267a2
vatsalya25 <vatsalya25@gmail.com>
2016-01-23 09:01:38 +0530

    CSS changes

--- src/app/board/board.controller.js
+++ src/app/board/board.controller.js
@@ -8,59 +8,56 @@ export class BoardController {
 
     var userRef = new Firebase("https://vyapi.firebaseio.com/messages");
     var authData = userRef.getAuth();
-    // userRef.child("messages/"+ roomID).set(this.messages);
 
-    // if (authData) {
-    //   console.log("User " + authData.uid + " is logged in with " + authData.provider);
-    //   // console.log(authData);
-    // }
-    // else {
-    //   console.log("User is logged out");
-    // }
-
-    var random = "https://vyapi.firebaseio.com/messages/" + roomID;
-    // var ref3 = new Firebase("https://vyapi.firebaseio.com/");
-    this.msgRef = new Firebase(random);
+    var roomURL= "https://vyapi.firebaseio.com/messages/" + roomID;
+    this.msgRef = new Firebase(roomURL);
     this.messages = $firebaseArray(this.msgRef);
-    // console.log(roomID);
-    // console.log(this.messages);
 
-    // userRef.child("messages/"+ roomID).set({
+    //CODE TO ENTER THE OBJECT IN FIREBASE DATABASE
+    this.submit = function(id) {
+      var userName = authData.google.displayName;
+      var userMessage = (id=='plus') ? this.userMessagePlus : this.userMessageMinus;
 
-      this.submit = function(id) {
+      if (userMessage) {
+        if(!userName) userName = "anonymous";
+        // console.log(authData);
 
-        var userName = authData.google.displayName;
-        var userMessage = (id=='plus') ? this.userMessagePlus : this.userMessageMinus;
+        this.messages.$add({
+          from: userName,
+          uid: authData.uid,
+          text: userMessage,
+          lane: id
+        });
 
-        if (userMessage) {
-          if(!userName) userName = "anonymous";
-          console.log(authData);
+        this.userMessagePlus = '';
+        this.userMessageMinus = '';
+      }
+    };
 
-          this.messages.$add({
-            from: userName,
-            uid: authData.uid,
-            text: userMessage,
-            lane: id
-          });
-
-          this.userMessagePlus = '';
-          this.userMessageMinus = '';
-        }
-      };
-
-
-      this.delete=function(msg){
-        var ide=msg.$id;
-        //  console.log(msg.uid);
-        //console.log(authData.google.id);
-        // var roomRef = new Firebase('https://vyapi.firebaseio.com/messages/'+roomID).remove();
-        if(msg.uid === "google:"+authData.google.id)
+    //CODE TO DELETE THE MESSAGE POSTED
+    this.delete=function(msg){
+      var ide=msg.$id;
+      //  console.log(msg.uid);
+      //console.log(authData.google.id);
+      // var roomRef = new Firebase('https://vyapi.firebaseio.com/messages/'+roomID).remove();
+      if(msg.uid === "google:"+authData.google.id)
         this.msgRef.child(ide).remove();
-        console.log(msg.$id);
-      };
+      console.log(msg.$id);
+    };
 
-      $('#anonymousWarn').fadeIn().delay(5000).fadeOut();
+    //CODE TO DISPLAY THE 5 SECOND NOTIFICATION FOR ANONYMITY
+    $('#anonymousWarn').fadeIn().delay(10000).fadeOut();
 
+    /*//CODE TO ENTER NEW LINE ON PRESSING SHIFT+ENTER
+    $('#userTextPlus').on('keydown', function(event) {
+      if (event.keyCode == 13)
+        if (!event.shiftKey) $('#testForm').submit();
+    });*/
 
+    //CODE TO MAKE THE USER ANONYMOUS
+
+    this.anonymous = function() {
+      userName = "anonymous";
     }
   }
+}
