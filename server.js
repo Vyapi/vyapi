25915e8f6dd0b942c351e10d2ea0dfacd9cc8f7a
Asheesh Pathak <eroinroderigo@gmail.com>
2016-01-25 18:06:05 +0530

    server.js

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+app.set('port', (process.env.PORT || 5000));
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: path.join(__dirname, '../vyapi/dist') });
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
