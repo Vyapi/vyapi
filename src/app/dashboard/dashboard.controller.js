bc10bafb2c55a80748b08a2bd2ff5f76b8a14ff2
monicagangwar <monicagangwar10@gmail.com>
2016-01-25 12:14:31 +0530

    refactored

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -10,36 +10,36 @@ export class DashboardController {
     };
     this.removeRoom = function(roomKey){
     	console.log(roomKey);
-			this.remove(Dashboard,roomKey);
-			};
-		}
-    set_param(Dashboard){
-    	let userID = Dashboard.getUserID();
+     this.remove(Dashboard,roomKey);
+ };
+}
+set_param(Dashboard){
+   let userID = Dashboard.getUserID();
 
-    	Dashboard.getRooms(userID).on("value",(snapshot)=>{
-    		let rooms = snapshot.val();
-    		rooms = _.map(rooms,(room,key)=>{
-    			room.key = key;
-    			room.url = this.path + '/' +  key;
+   Dashboard.getRooms(userID).on("value",(snapshot)=>{
+      let rooms = snapshot.val();
+      rooms = _.map(rooms,(room,key)=>{
+         room.key = key;
+         room.url = this.path + '/' +  key;
 				//console.log(room.url);
 				return room;
 			});
-    		this.rooms = rooms;
-    		console.table(this.rooms);
-    	});
-    }
-    create(Dashboard){
-    	let userID = Dashboard.getUserID();
-    	let random = Dashboard.createRoom(userID);
-    }
-		remove(Dashboard,roomKey){
-			Dashboard.remove(roomKey);
-		}
+      this.rooms = rooms;
+      console.table(this.rooms);
+  });
+}
+create(Dashboard){
+   let userID = Dashboard.getUserID();
+   let random = Dashboard.createRoom(userID);
+}
+remove(Dashboard,roomKey){
+ Dashboard.remove(roomKey);
+}
 
-    firebaseAuthlogout() 
-    {
-    	let ref = new Firebase("https://vyapi.firebaseio.com");
-    	ref.unauth();
-    	window.location.href='/';
-    }
+firebaseAuthlogout() 
+{
+   let ref = new Firebase("https://vyapi.firebaseio.com");
+   ref.unauth();
+   window.location.href='/';
+}
 }
