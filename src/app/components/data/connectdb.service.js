32fe651750fcde0f109d87ef8e61ec52ced0b8fe
Mayuresh Waykole <mayuresh2212@gmail.com>
2016-01-26 01:28:26 +0530

    added 2 levels of abstraction before controller

--- src/app/components/data/connectdb.service.js
+++ src/app/components/data/connectdb.service.js
@@ -1,15 +1,18 @@
 export class FireConnect{
   constructor($firebaseAuth,$log){
     'ngInject';
+    this.fa = $firebaseAuth;
     this.firebaseURL = "https://vyapi.firebaseio.com";
-    this.ref = new Firebase("https://vyapi.firebaseio.com");
-    this.authObj = $firebaseAuth(this.ref);
+    this.ref = null
+    this.authObj = null
     this.authData = this.authObj.$getAuth();
     this.clog = $log;
   }
 
-  connect(){
-    this.clog.log("authObject returned, connection made");
+  connect(path='/'){
+    this.ref = new Firebase("https://vyapi.firebaseio.com"+path);
+    this.authObj = this.fa(this.ref);
+    this.clog.log("authObject returned, connection made to "+path);
     return this.authObj;
   }
 
--- src/app/components/data/data.service.js
+++ src/app/components/data/data.service.js
@@ -2,7 +2,47 @@ export class FireData{
   constructor(FireConnect,$log){
     'ngInject';
     this.fc = FireConnect;
+    this.authObj = null;
+    this.authData = this.authObj.$getAuth();
+    // for rooms
+    this.roomPath = '/rooms';
+    this.messagesPath = '/messages';
   }
-  
+
+  authExist(path='/'){
+    var authObjLocal = this.fc.connect(path);
+    var authDataLocal = authObjLocal.$getAuth();
+    if (authDataLocal) {
+      return authDataLocal;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  getUid(){
+    var authDataLocal = authExist();
+    return authDataLocal.uid;
+  }
+
+  getRooms(userID){
+    var ref = this.fc.connect(this.roomPath);
+    return ref.orderByChild("ownedBy").equalTo(userID);
+  }
+
+  createRoom(userID){
+    var ref = this.fc.connect(this.roomPath);
+    let roomName=prompt('Room Name','Default');
+    if(!roomName){
+      alert('Please enter a valid room name');
+    }
+    else{
+      let newRoom = ref.push({roomName : roomName, ownedBy : userID});
+      return newRoom;
+    }
+  }
+
+
 
 }
