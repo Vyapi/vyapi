1e3dcd9256cf0b5e7b31d68f75ee51b9e1997d5b
srishti31 <srishti@Admins-MacBook-Pro-2.local>
2016-01-26 16:36:51 +0530

    Edit using modal popup

--- src/app/board/board.controller.js
+++ src/app/board/board.controller.js
@@ -1,9 +1,8 @@
 export class BoardController {
-  constructor ($firebaseArray, $location) {
+  constructor ($scope, $firebaseArray, $location) {
     'ngInject';
-
+    $scope.select=null;
     var userName = "";
-    var appURL = "https://vyapi.firebaseio.com/";
     var roomID = $location.path().split("/")[2];
     console.log("roomID is " + roomID);
     this.likes = [];
@@ -19,16 +18,22 @@ export class BoardController {
     // $(this).
 
     //CODE TO MAKE THE USER ANONYMOUS
-    var anonymous = true;
+    /*$('#anonymousTogglePlus').toggle(function(){
+        $(this).addClass("active");
+        }, function () {
+        $(this).removeClass("active");
+    });*/
 
+    var anonymous = true;
+   
     this.toggle = function() {
       anonymous = !anonymous;
       if(!anonymous){
         userName = authData.google.displayName;
-        $('.anonymous-toggle').css({"background-color":"#eeeeee","color":"black"});
+        $('.anonymousToggle').css({"background-color":"#eeeeee","color":"black"});
       } else {
         userName = "anonymous";
-        $('.anonymous-toggle').css({"background-color":"#6D6A68","color":"white"});
+        $('.anonymousToggle').css({"background-color":"#6D6A68","color":"white"});
       }
       console.log(userName);
     };
@@ -48,22 +53,13 @@ export class BoardController {
           lane: id
         });
 
-        //CODE TO GET USER PHOTO FOR DISPLAY ON BOTTOM OF STICKY
-        (new Firebase(encodeURI(appURL + "users/" + authData.uid + "/google/profileImageURL/"))).once("value", (value) => {
-          var profileImageURL = value.val();
-          console.log("USER PHOTO");
-          return profileImageURL;
-        });
-
-        var roomURL= "https://vyapi.firebaseio.com/users/google%3A" + authData.uid + "/google/profileImageURL";
-
         this.userMessagePlus = '';
         this.userMessageMinus = '';
       }
     };
 
     //CODE TO DELETE THE MESSAGE POSTED
-    this.delete = function(msg){
+    this.delete=function(msg){
       var ide=msg.$id;
       //  console.log(msg.uid);
       //console.log(authData.google.id);
@@ -76,59 +72,48 @@ export class BoardController {
     //CODE TO DISPLAY THE 5 SECOND NOTIFICATION FOR ANONYMITY
     $('#anonymousWarn').fadeIn().delay(5000).fadeOut();
 
-    /*//CODE TO ENTER NEW LINE ON PRESSING SHIFT+ENTER
-    $('#userTextPlus').on('keydown', function(event) {
-      if (event.keyCode == 13)
-        if (!event.shiftKey) $('#testForm').submit();
-    });*/
 
-    //if message added, add a listener for the number of likes
-    //todo: optimization. the listeners still live even if the corresponding messsage is deleted
-    (new Firebase(roomURL)).on('value', (messagesObj) => {
-      for (var messageId in messagesObj.val()) {
-        (new Firebase(roomURL + "/" + messageId + "/like/")).on("value", (userId) => {
-          if(userId.val() != null) {
-            console.log("user: " + Object.keys(userId.val()) + " liked " + messageId);
-            if(this.likes[messageId] == undefined)
-              this.likes[messageId] = 0;
-            this.likes[messageId] = this.likes[messageId] +1;
-          }
-        });
+
+    
+    this.isOwner=function(msg)
+    {
+       var ide=msg.$id;
+      if(msg.uid=== "google:"+authData.google.id)
+         return true;
+       else
+        return false;
+
+    }
+    this.like = function(msg) {
+      console.table("like: " + msg.$id);
+      //(new Firebase(roomURL)).child(msg.$id + "/like/" +authData.uid).set(1);
+      var t = (new Firebase(roomURL)).child(msg.$id + "/like/" +authData.uid);
+      t.once("value" , function(value){
+        console.log('triggring event');
+      if(value.exists()){
+        t.remove();
+      }
+      else{
+        t.set(1);
+        t.off();
       }
     });
-
-    //handle like button click for a message
-    this.like = function(msg) { //index is the index in this.messages array
-      var text = msg.$id;
-      console.table("like: " + msg.$id);
-      (new Firebase(roomURL)).child(text + "/like/" +authData.uid).set(1);
+       var ta = (new Firebase(roomURL)).child(msg.$id + "/like/");
+     ta.once('value', function(snapshot) {console.log(snapshot.numChildren()); });
     }
 
-    //Function to retrieve likes for a sticky
-    this.getLikeCount = function (msg){
-      console.log(this.likes);
-      return this.likes[msg.$id];
+    this.currentMessageText= "hello";
+    this.currentMessage;
+    this.edit = function(msg) {
+      console.log("edit hererere");
+      this.currentMessageText= msg.text;
+      this.currentMessage = msg;
     }
-
-    //CODE TO ENABLE DRAG AND DROP OF STICKYs
-    $(function() {
-      $('.delete-btn').css({'display' : 'none'});
-      $('.like-btn').css({'display' : 'none'});
-      $("#chat-messages-plus").sortable();
-      $("#chat-messages-minus").sortable();
-      $("#chat-messages-plus").disableSelection();
-      $("#chat-messages-minus").disableSelection();
-    });
-
-    //CODE TO SHOW DELETE/LIKE ETC ON HOVER
-    this.hover = function(){
-      $('.delete-btn').css({'display' : 'inline-block'});
-      $('.like-btn').css({'display' : 'inline-block'});
-    };
-
-    this.show = function(){
-      $('.delete-btn').css({'display' : 'none'});
-      $('.like-btn').css({'display' : 'none'});
-    };
+    this.saveEdit = function ()
+    {
+      (new Firebase(roomURL + "/" + this.currentMessage.$id+"/text")).set( this.currentMessageText);
+      console.log( this.currentMessageText + "TT") ;
+    }
   }
+  
 }
