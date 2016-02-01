dc5a9decfacefd9546ddc7402465cde81e6719e7
ayushinigam <ayushi@admins-MacBook-Pro-10.local>
2016-02-01 17:21:16 +0530

    Loading of dashboard rooms on Home button click -- Bug fixed

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -2,8 +2,6 @@ export class DashboardController {
 	constructor ($firebaseArray,Auth,Dashboard,$location,$log,$window,$cookies){
 		'ngInject';
 		this.path = $location.absUrl().replace('dashboard', 'room');
-		this.cards(Dashboard);
-		this.setParam(Dashboard);
 		this.location = $location;
 		this.cookies = $cookies;
 		this.auth = Auth;
@@ -20,6 +18,9 @@ export class DashboardController {
 		this.mName='';
 		this.pName='';
 		this.editKey='';
+		this.cards(Dashboard);
+		this.setParam(Dashboard);
+
 		this.createRoom = function(){
 			if(!this.roomName)
 			{
@@ -69,9 +70,12 @@ export class DashboardController {
 
 	setParam(Dashboard){
 		if(!Dashboard)
+		{
 			return;
+		}
 		let userID = Dashboard.getUserID();
 		if(!userID){
+			console.log("In !userid");
 			this.rooms = ["mock data"];
 			return "set param is being called";
 		}
