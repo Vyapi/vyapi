b70d73ce83b3b670857196f3bf7a56275f10932b
Asheesh Pathak <eroinroderigo@gmail.com>
2016-02-02 16:42:48 +0530

    heroku

--- gulp/build.js
+++ gulp/build.js
@@ -71,10 +71,16 @@ gulp.task('html', ['inject', 'partials'], function () {
 // Only applies for fonts from bower dependencies
 // Custom fonts are handled by the "other" task
 gulp.task('fonts', function () {
+
   return gulp.src($.mainBowerFiles())
     .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
     .pipe($.flatten())
     .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
+});
+
+gulp.task('copy', function() {
+   gulp.src('server.js')
+   .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('other', function () {
@@ -94,4 +100,4 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other']);
+gulp.task('build', ['html', 'fonts', 'other','copy']);
