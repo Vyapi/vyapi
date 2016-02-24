831121b5a1b283965daca82468ecb2209bab70a5
diwakar-s-maurya <diwakar-s-maurya@users.noreply.github.com>
2016-02-24 16:15:36 +0530

    Revert "Make lanes responsive"

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -99,33 +99,31 @@ export function routerConfig ($stateProvider, $urlRouterProvider,$locationProvid
     url: '/room/:roomKey',
     views: {
 
-      '': {
-        templateUrl: 'app/room/room.html',
-        controller:'RoomController',
-        controllerAs: 'room'
-      },
-      // the child views will be defined here (absolutely named)
-      'board@room': {
-        templateUrl: 'app/board/board.html',
+        '': { templateUrl: 'app/room/room.html',
+          controller:'RoomController',
+          controllerAs: 'room'},
+        // the child views will be defined here (absolutely named)
+        'board@room': { templateUrl: 'app/board/board.html',
         controller: 'BoardController',
         controllerAs: 'board'
-      },
+        },
 
-      //user room view
-      'user@room': {
-        templateUrl: 'app/user/user.html',
-        controller: 'UserController',
-        controllerAs: 'users'
-      },
+    //user room view
+    'user@room': {
+      templateUrl: 'app/user/user.html',
+      controller: 'UserController',
+      controllerAs: 'users'
 
-      //action room view
-      'action@room':{
-        templateUrl: 'app/action/action.html',
-        controller: 'ActionController',
-        controllerAs: 'action'
-      }
+    },
+
+    //action room view
+    'action@room':{
+      templateUrl: 'app/action/action.html',
+      controller: 'ActionController',
+      controllerAs: 'action'
     }
-  });
-  $locationProvider.html5Mode(true);
-  $urlRouterProvider.otherwise('/');
+  }
+});
+$locationProvider.html5Mode(true);
+$urlRouterProvider.otherwise('/');
 }
