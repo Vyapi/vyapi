d9358c01a17ecddf9f1eff34b8401052b36251fa
diwakar <diwakar.s.maurya@gmail.com>
2016-03-06 02:58:28 +0530

    user generic redirect url

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,11 +1,12 @@
 export class LoginController{
-  constructor($log,Auth,FireData, $state, $http, $localStorage){
+  constructor($log,Auth,FireData, $state, $http, $localStorage, $location){
     'ngInject';
     this.auth = Auth;
     this.clog = $log;
     this.fd = FireData;
     this.state = $state;
     this.http = $http;
+    this.location = $location;
 
     //check if user is logged in, and redirect accordingly
     var auth = this.fd.getAuthData();
@@ -16,7 +17,6 @@ export class LoginController{
     this.googleAuthLogout = function() {
       delete $localStorage['id'];
     }
-
   }
 
   firebaseAuthLogin(){
@@ -39,7 +39,7 @@ export class LoginController{
   googleAuthLogin() {
     var client_id="662183359983-j65e9u3vetanp8n779oqohvkc55a1s4h.apps.googleusercontent.com";
     var scope="email profile";
-    var redirect_uri="http://localhost:3000/googleAuth";
+    var redirect_uri=this.location.protocol() + '://'+ this.location.host() +':'+  this.location.port() + "/googleAuth";
     var response_type="token";
     var url="https://accounts.google.com/o/oauth2/auth?scope="+scope+"&client_id="+client_id+"&redirect_uri="+redirect_uri+
     "&response_type="+response_type;
