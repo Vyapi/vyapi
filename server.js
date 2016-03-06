c8d642164f3ad39e4be7dec4f5ed1bb45efad70d
diwakar <diwakar.s.maurya@gmail.com>
2016-03-06 16:43:16 +0530

    use nodemailer

--- server.js
+++ server.js
@@ -6,28 +6,49 @@ var gmailConf = require('./credentials.js');
 app.set('port', (process.env.PORT || 5001));
 app.use(express.static(__dirname+'/dist'));
 
+console.log(gmailConf.GMAIL_USERNAME);
+console.log(gmailConf.GMAIL_PASSWORD);
+console.log(gmailConf.GMAIL_FROM);
+console.log(gmailConf.GMAIL_HOST);
+
 //send email
 app.get('/emailWrite', function(req, res) {
-  var email   = require("emailjs/email");
-  var server  = email.server.connect({
-      user:    gmailConf.GMAIL_USERNAME,
-      password:gmailConf.GMAIL_PASSWORD,
-      host:    gmailConf.GMAIL_HOST,
-      ssl:     true
+
+  var nodemailer = require('nodemailer');
+
+  var poolConfig = {
+    pool: true,
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+        user: gmailConf.GMAIL_USERNAME,
+        pass: gmailConf.GMAIL_PASSWORD
+    }
+  };
+
+  // create reusable transporter object using the default SMTP transport
+  var transporter = nodemailer.createTransport(poolConfig);
+
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+      from: gmailConf.GMAIL_FROM, // sender address
+      to: req.query.to, // list of receivers
+      subject: req.query.subject, // Subject line
+      generateTextFromHTML: true,
+      html: req.query.text, // html body
+      //text: req.query.text, // plaintext body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error) {
+          res.status(500).send(error);
+          return console.log(error);
+      }
+      console.log('Message sent: ' + info.response);
+      res.status(200).send('done');
   });
-
-  server.send({
-    text:    req.query.text,
-    from:    gmailConf.GMAIL_FROM,
-    to:      req.query.to,
-    subject: req.query.subject
-  }, function(err, message) {
-    if(err != null)
-      console.log(err); //console.log(message); //message which got sent to SMTP
-  })
-  console.log('email process complete');
-
-  res.status(200).send('done');
 });
 
 app.get('/*', function (req, res) {
